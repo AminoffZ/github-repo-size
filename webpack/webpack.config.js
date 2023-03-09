df77901d0338f2dac0025a29f7cf230254611b0f
AminoffZ <55880031+AminoffZ@users.noreply.github.com>
2023-03-09 23:59:33 +0200

    Initial commit

---
+++ webpack/webpack.config.js
@@ -0,0 +1,31 @@
+// This file is used to build the extension for production
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+module.exports = {
+  mode: "production",
+  entry: {
+    content: path.resolve(__dirname, "..", "src/scripts", "content.ts"),
+    script: path.resolve(__dirname, "..", "src/scripts", "mute-button.ts"),
+  },
+  output: {
+    path: path.join(__dirname, "../dist"),
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: ".", to: ".", context: "public" }],
+    }),
+  ],
+};
