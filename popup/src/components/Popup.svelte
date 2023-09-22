dc8adb0fe416c779f46291c9913a5bc06ffed104
AminoffZ <55880031+AminoffZ@users.noreply.github.com>
2023-09-22 15:17:19 +0300

    Init

---
+++ popup/src/components/Popup.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  let counter = 0;
+  onMount(() => {
+    console.log('mounted');
+  });
+</script>
+
+<div>
+  <h1>hello</h1>
+  <h1>yo</h1>
+  <button on:click={() => counter++}>{counter}</button>
+</div>
