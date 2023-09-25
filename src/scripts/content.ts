e0f1e82c3312d454e03c203ca5cf39b08dacbf8e
AminoffZ <mouiylus@gmail.com>
2023-09-25 13:40:39 +0300

    🚆 Overhaul
    next nested sizes

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,96 +1,47 @@
-import {
-  API,
-  storage,
-  OAUTH_TOKEN_KEY,
-  GITHUB_TOKEN_KEY,
-  NAV_ELEM_ID,
-  FILE_BUTTON,
-} from '../constants';
+import { GITHUB_TOKEN_KEY, OAUTH_TOKEN_KEY, storage } from '../constants';
 
-interface RepoObject {
-  repo: string;
-  ref: string | undefined;
-  currentPath: string;
+type PathObject = {
+  owner: string | undefined;
+  repo: string | undefined;
+  type?: string | undefined;
+  branch?: string | undefined;
+  path?: string | undefined;
+};
+
+type GitHubTreeItem = {
+  path: string;
+  mode: string;
+  type: string;
+  sha: string;
+  size?: number;
+  url: string;
+};
+
+type GitHubTree = {
+  sha: string;
+  url: string;
+  tree: GitHubTreeItem[];
+  truncated: boolean;
+};
+
+function getPathObject() {
+  const pathObject = {};
+  try {
+    const paths = window.location.pathname.split('/');
+    Object.assign(pathObject, {
+      owner: paths.at(1),
+      repo: paths.at(2),
+      type: paths.at(3),
+      branch: paths.at(4),
+      path: paths.slice(5)?.join('/'),
+    });
+  } catch (e) {
+    console.error(e);
+  }
+  return pathObject as PathObject;
 }
 
-const getRepoObject = (): RepoObject | undefined => {
-  const elem = document.querySelector(FILE_BUTTON) as HTMLAnchorElement;
-  if (!elem) return;
-
-  if (!elem.href || !elem.href.match(/^https?:\/\/github.com\//)) {
-    return;
-  }
-
-  const repoUri = elem.href.replace(/^https?:\/\/github.com\//, '');
-  const arr = repoUri.split('/');
-  const userOrg = arr.shift();
-  const repoName = arr.shift();
-  const repo = `${userOrg}/${repoName}`;
-  arr.shift();
-  const ref = arr.shift();
-
-  return {
-    repo,
-    ref,
-    currentPath: arr.join('/').trim(),
-  };
-};
-
-interface HumanReadableSizeObject {
-  size: number;
-  measure: string;
-}
-
-const getHumanReadableSizeObject = (bytes: number): HumanReadableSizeObject => {
-  if (bytes === 0) {
-    return {
-      size: 0,
-      measure: 'Bytes',
-    };
-  }
-
-  const K = 1024;
-  const MEASURE = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-  const i = Math.floor(Math.log(bytes) / Math.log(K));
-
-  return {
-    size: parseFloat((bytes / Math.pow(K, i)).toFixed(2)),
-    measure: MEASURE[i],
-  };
-};
-
-const getHumanReadableSize = (size: number | undefined): string => {
-  if (!size) return '';
-  const t = getHumanReadableSizeObject(size);
-  return t.size + ' ' + t.measure;
-};
-
-const getSizeHTML = (size: number | undefined): string => {
-  const humanReadableSize = getHumanReadableSizeObject(size || 0);
-  return `
-<li class="d-flex" id="${NAV_ELEM_ID}">
-  <a class="js-selected-navigation-item UnderlineNav-item hx_underlinenav-item no-wrap js-responsive-underlinenav-item" data-tab-item="settings-tab" style="cursor: pointer">
-    <svg class="octicon octicon-gear UnderlineNav-octicon d-none d-sm-inline" display="none inline" aria-hidden="true" height="16" version="1.1" viewBox="0 0 12 16" width="12">
-    <path d="M6 15c-3.31 0-6-.9-6-2v-2c0-.17.09-.34.21-.50.67.86 3 1.5 5.79 1.5s5.12-.64 5.79-1.5c.13.16.21.33.21.50v2c0 1.1-2.69 2-6 2zm0-4c-3.31 0-6-.9-6-2V7c0-.11.04-.21.09-.31.03-.06.07-.13.12-.19C.88 7.36 3.21 8 6 8s5.12-.64 5.79-1.5c.05.06.09.13.12.19.05.10.09.21.09.31v2c0 1.1-2.69 2-6 2zm0-4c-3.31 0-6-.9-6-2V3c0-1.1 2.69-2 6-2s6 .9 6 2v2c0 1.1-2.69 2-6 2zm0-5c-2.21 0-4 .45-4 1s1.79 1 4 1 4-.45 4-1-1.79-1-4-1z"></path>
-    </svg>
-    <span data-content="Settings">${humanReadableSize.size} ${humanReadableSize.measure}</span>
-  </a>
-</li>
-`;
-};
-
-const checkStatus = (response: Response): Response => {
-  if (response.status >= 200 && response.status < 300) {
-    return response;
-  }
-  console.error(response);
-  throw new Error(`GitHub returned an invalid status: ${response.status}`);
-};
-
-const getAPIData = async (uri: string): Promise<any> => {
-  const headerObj: { [key: string]: string } = {
-    'User-Agent': 'AminoffZ/github-repo-size',
-  };
+async function getToken() {
   const oauthToken: string | undefined = (await storage.get(OAUTH_TOKEN_KEY))[
     OAUTH_TOKEN_KEY
   ];
@@ -98,155 +49,117 @@ const getAPIData = async (uri: string): Promise<any> => {
     GITHUB_TOKEN_KEY
   ];
   const token = githubToken || oauthToken;
+  return token;
+}
+
+async function createRequest(repo: string) {
+  const headers = new Headers();
+  headers.append('User-Agent', 'AminoffZ/github-repo-size');
+  const token = await getToken();
   if (token) {
-    headerObj.Authorization = 'Bearer ' + token;
+    headers.append('Authorization', `Bearer ${token}`);
   }
-  const request = new Request(`${API}${uri}`, {
-    headers: new Headers(headerObj),
-  });
-  return fetch(request)
-    .then(checkStatus)
-    .then((response) => response.json());
-};
-
-const getFileName = (text: string): string => text.trim().split('/')[0];
-
-const checkForRepoPage = async (): Promise<void> => {
-  const repoObj = getRepoObject();
-  if (!repoObj) return;
-  await new Promise<void>((resolve, reject) => {
-    function loading(): boolean {
-      return !!document.querySelector('div[role="gridcell"] div.Skeleton');
+  const request = new Request(
+    `https://api.github.com/repos/${repo}/git/trees/main?recursive=1`,
+    {
+      headers,
     }
+  );
+  return request;
+}
 
-    if (!loading()) return resolve();
-
-    const interval = setInterval(() => {
-      if (!loading()) {
-        clearInterval(interval);
-        resolve();
-      }
-    }, 100);
-  });
-
-  const ns = document.querySelector('ul.UnderlineNav-body');
-  const navElem = document.getElementById(NAV_ELEM_ID);
-  const tdElems = document.querySelector('span.github-repo-size-div');
-
-  if (ns && !navElem) {
-    getAPIData(repoObj.repo).then((summary) => {
-      if (summary && summary.size) {
-        ns.insertAdjacentHTML('beforeend', getSizeHTML(summary.size * 1024));
-        const newLiElem = document.getElementById(NAV_ELEM_ID);
-        if (!newLiElem) return;
-        newLiElem.title = 'Click to load directory sizes';
-        newLiElem.style.cssText = 'cursor: pointer';
-        newLiElem.onclick = loadDirSizes;
-      }
+async function getRepoInfo(repo: string) {
+  const request = await createRequest(repo);
+  const response = await fetch(request)
+    .then(async (res) => {
+      const data = await res.json();
+      console.log(data);
+      return data as GitHubTree;
+    })
+    .catch((err) => {
+      console.log(err);
+      return undefined;
     });
+  return response;
+}
+
+function getAnchors() {
+  const anchors = document.querySelectorAll('a.Link--primary');
+  return anchors as NodeListOf<HTMLAnchorElement>;
+}
+
+function formatBytes(bytes: number, decimals: number = 2): string {
+  if (bytes === 0) return '0 B';
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+function getNestedSize(
+  treeItem: GitHubTreeItem,
+  tree: GitHubTreeItem[]
+): number {
+  if (treeItem.type === 'blob') {
+    return treeItem.size ?? 0;
+  } else if (treeItem.type === 'tree') {
+    const nestedTree = tree.filter((item) => {
+      return item.path.startsWith(treeItem.path + '/');
+    });
+    const size = nestedTree.reduce((acc, cur) => {
+      return acc + getNestedSize(cur, tree);
+    }, 0);
+    treeItem.size = size;
+    return size;
+  } else {
+    return 0;
   }
+}
 
-  if (tdElems) return;
-
-  const tree = await getAPIData(
-    `${repoObj.repo}/contents/${repoObj.currentPath}?ref=${repoObj.ref}`
-  );
-  const sizeObj: Record<string, string | number> = { '..': '..' };
-
-  tree.forEach((item: any) => {
-    sizeObj[item.name] = item.type !== 'dir' ? item.size : 'dir';
-  });
-
-  const list = Array.from(document.querySelectorAll('div[role="row"].Box-row'));
-  const items = Array.from(
-    document.querySelectorAll('div[role="row"].Box-row div[role="rowheader"] a')
-  );
-
-  const ageForReference = document.querySelectorAll(
-    'div[role="row"].Box-row div[role="gridcell"]:last-child'
-  );
-  items.forEach((item: any, index) => {
-    if (!item.text) return;
-    const filename = getFileName(item.text);
-    const t = sizeObj[filename];
-    const div = document.createElement('div');
-    div.setAttribute('role', 'gridcell');
-    div.style.cssText = 'width: 80px';
-    div.className = 'text-gray-light text-right mr-3';
-    let label;
-    if (t === 'dir') {
-      label = '&middot;&middot;&middot;';
-      div.className += ' github-repo-size-dir';
-      div.title = 'Click to load directory size';
-      div.style.cssText = 'cursor: pointer; width: 80px';
-      div.onclick = loadDirSizes;
-      div.setAttribute('data-dirname', filename);
-    } else if (t === '..') {
-      label = '';
-    } else {
-      label = getHumanReadableSize(t as number);
+async function main() {
+  const anchors = getAnchors();
+  if (!anchors || anchors.length === 0) {
+    return;
+  }
+  console.log(anchors);
+  const pathObject = getPathObject();
+  if (!pathObject) {
+    return;
+  }
+  console.log(pathObject);
+  const repoInfo = await getRepoInfo(pathObject.owner + '/' + pathObject.repo);
+  if (!repoInfo) {
+    return;
+  }
+  console.log(repoInfo);
+  anchors.forEach((anchor) => {
+    const path = anchor.getAttribute('href');
+    if (!path) {
+      return;
     }
-    div.innerHTML = `<span class="css-truncate css-truncate-target d-block width-fit github-repo-size-div">${label}</span>`;
-    list[index].insertBefore(div, ageForReference[index]);
-  });
-};
-
-const loadDirSizes = async (): Promise<void> => {
-  const filesNodeList = document.querySelectorAll(
-    'div[role="row"].Box-row div[role="rowheader"] a'
-  );
-  const files = Array.from(filesNodeList);
-
-  const dirSizesNodeList = document.querySelectorAll(
-    'div.github-repo-size-dir span'
-  );
-  const dirSizes = Array.from(dirSizesNodeList);
-
-  const navElem = document.getElementById(NAV_ELEM_ID);
-  if (navElem) {
-    navElem.onclick = null;
-    navElem.title = 'Loading directory sizes...';
-  }
-  dirSizes.forEach((dir) => {
-    dir.textContent = '...';
-    dir.parentElement!.onclick = null;
-  });
-  const repoObj = getRepoObject();
-  if (!repoObj) return;
-  const data = await getAPIData(
-    `${repoObj.repo}/git/trees/${repoObj.ref}?recursive=1`
-  );
-  if (data.truncated) {
-    console.warn(
-      'github-repo-size: Data truncated. Directory size info may be incomplete.'
-    );
-  }
-  const sizeObj: Record<string, number> = {};
-  data.tree.forEach((item: any) => {
-    if (!item.path.startsWith(repoObj.currentPath)) return;
-    const arr = item.path
-      .replace(new RegExp(`^${repoObj.currentPath}`), '')
-      .replace(/^\//, '')
-      .split('/');
-    if (arr.length >= 2 && item.type === 'blob') {
-      const dir = arr[0];
-      if (sizeObj[dir] === undefined) sizeObj[dir] = 0;
-      sizeObj[dir] += item.size;
+    const pathArray = path.split('/');
+    const pathName = pathArray[pathArray.length - 1];
+    const treeItem = repoInfo.tree.find((item) => item.path === pathName);
+    if (!treeItem) {
+      return;
     }
-  });
-  files.forEach((file) => {
-    const dirname = getFileName(file.textContent || '');
-    const t = sizeObj[dirname];
-    const dir = dirSizes.find(
-      (dir2) => dir2.parentElement!.getAttribute('data-dirname') === dirname
-    );
-    if (dir) {
-      dir.textContent = getHumanReadableSize(t as number);
+    let size = treeItem.size;
+    if (!size) {
+      size = getNestedSize(treeItem, repoInfo.tree);
     }
-  });
-  if (navElem) {
-    navElem.title = 'Directory sizes loaded successfully';
-  }
-};
+    const sizeString = formatBytes(size);
+    const span = document.createElement('span');
+    span.innerText = sizeString;
+    const row = anchor.parentElement?.parentElement?.parentElement;
+    const div = row?.childNodes[row.childNodes.length - 2].cloneNode(false);
+    div?.appendChild(span);
+    if (!div) {
+      return;
+    }
 
-checkForRepoPage();
+    row?.insertBefore(div, row.childNodes[row.childNodes.length - 2]);
+  });
+}
+
+main();
