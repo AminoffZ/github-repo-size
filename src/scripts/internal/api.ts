0e5566c6f2a8e5b61cf5c33d7a47dae5357f00bb
AminoffZ <mouiylus@gmail.com>
2023-09-27 16:01:31 +0300

    🎋 Default Branch Handling
    Handle cases where default branch is not 'main'.

--- src/scripts/internal/api.ts
+++ src/scripts/internal/api.ts
@@ -18,15 +18,37 @@ async function createTreeRequest(repo: string, branch: string) {
   return request;
 }
 
-export async function getRepoInfo(repo: string, branch: string = 'main') {
+async function getDefaultBranch() {
+  let branch = '';
+  await fetch('https://api.github.com/repos/harshjv/github-repo-size')
+    .then(async (res) => {
+      const data = await res.json();
+      branch = data.default_branch;
+    })
+    .catch(async (err) => console.error(err));
+  return branch;
+}
+
+export async function getRepoInfo(
+  repo: string,
+  branch: string = 'main',
+  attempts: number = 0
+): Promise<GitHubTree | undefined> {
   const request = await createTreeRequest(repo, branch);
   const response = await fetch(request)
     .then(async (res) => {
+      if (!res.ok && res.status === 404 && res.type === 'cors') {
+        if (attempts < 1) {
+          const defaultBranch = await getDefaultBranch();
+          return getRepoInfo(repo, defaultBranch, attempts + 1);
+        }
+      }
+
       const data = await res.json();
       return data as GitHubTree;
     })
     .catch((err) => {
-      console.error(err);
+      console.error(err.message);
       return undefined;
     });
   return response;
