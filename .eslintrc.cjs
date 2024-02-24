1005d217a29d42bf5835ac0d316b2760c62e3a0a
mAmineChniti <emin.chniti@esprit.tn>
2024-02-24 16:42:36 +0100

    updated versioning to include optional arg for what browser

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,5 +19,6 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint'],
+  ignorePatterns: ['node_modules/', 'github-repo-extension/'],
   rules: {},
 };
--- update-version.ts
+++ update-version.ts
@@ -1,10 +1,11 @@
-#!/usr/bin/env bun
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 const fs = require('fs').promises;
 const path = require('path');
 const chalk = require('chalk');
 async function updateManifestVersion() {
   try {
+    const arg = process.argv[2];
+    const browser = arg && arg === 'firefox' ? 'firefox' : 'chrome';
     const packageJsonPath = path.join(import.meta.dir, 'package.json');
     const { version } = await import(packageJsonPath);
 
@@ -14,9 +15,32 @@ async function updateManifestVersion() {
 
     manifest.version = version;
 
+    if (arg && arg === 'firefox') {
+      manifest.background = {
+        scripts: ['background.js'],
+      };
+
+      manifest.browser_specific_settings = {
+        gecko: {
+          id: 'mouiylus@gmail.com',
+          strict_min_version: '121.0.1',
+        },
+      };
+    } else {
+      manifest.background = {
+        service_worker: 'background.js',
+      };
+      delete manifest.browser_specific_settings;
+    }
     await fs.writeFile(manifestPath, JSON.stringify(manifest, null, 2));
 
-    console.log(chalk.green('Version updated successfully in manifest.json'));
+    console.log(
+      chalk.green(
+        'Version updated successfully in manifest.json for ' +
+          browser +
+          ' Browser!'
+      )
+    );
   } catch (error: any) {
     console.error(chalk.red('Error updating version: ' + error.message));
   }
