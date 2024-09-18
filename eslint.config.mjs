de11ed8a4fab9e2371dd3b917a32a7de02848196
AminoffZ <mouiylus@gmail.com>
2024-09-18 19:56:36 +0300

    Format eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,43 +1,51 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [{
-    ignores: ["**/node_modules/", "**/github-repo-size-extension/"],
-}, ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"), {
+export default [
+  {
+    ignores: ['**/node_modules/', '**/github-repo-size-extension/'],
+  },
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ),
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
+      '@typescript-eslint': typescriptEslint,
     },
 
     languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-        },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
 
-        parser: tsParser,
-        ecmaVersion: 2021,
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 2021,
+      sourceType: 'module',
     },
 
     rules: {},
-}, {
-    files: ["**/.eslintrc.{js,cjs}"],
+  },
+  {
+    files: ['**/.eslintrc.{js,cjs}'],
 
     languageOptions: {
-        ecmaVersion: 5,
-        sourceType: "script",
+      ecmaVersion: 5,
+      sourceType: 'script',
     },
-}];
+  },
+];
